63d6fd9b96aebb4ab3ec106e720e9dbffbca6982
Nam Hữu Nam <tranhuunam2000@gmail.com>
2025-03-31 19:45:03 +0700

    Add files via upload

---
+++ resources/views/themepmc/inc/slider_recommended.blade.php
@@ -0,0 +1,21 @@
+<div class="block top-slide">
+    <div class="heading">
+        <h2 class="caption" title="Xem Phim">Phim Đề Cử</h2>
+    </div>
+    <ul id="film-hot" class="list-film">
+        @foreach ($recommendations as $movie)
+            <li class="item">
+                <span class="label">
+                    <span class="film-format">{{$movie->episode_current}} | {{$movie->language}}</span>
+                </span>
+                <a title="{{$movie->name}}" href="{{$movie->getUrl()}}">
+                    <img alt="{{$movie->name}}" 
+                         class="lazyload" 
+                         data-src="{{$movie->getPosterUrl()}}" 
+                         src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" />
+                    <p>{{$movie->name}}</p> <i class="icon-play"></i>
+                </a>
+            </li>
+        @endforeach
+    </ul>
+</div>
